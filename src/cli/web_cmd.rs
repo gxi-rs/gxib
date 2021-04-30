8a4531b4c13377b1fce2149b933222dbbbcb1bdf
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-04-30 12:22:49 +0530

    fixes

--- src/cli/web_cmd.rs
+++ src/cli/web_cmd.rs
@@ -3,6 +3,6 @@ use clap::Clap;
 /// build for the web platform
 #[derive(Clap)]
 pub struct WebArgs {
-    #[clap(short, long, default_value = true)]
-    serve: bool
+    #[clap(short, long)]
+    pub serve: bool
 }
--- src/pipelines/web/web_pipeline.rs
+++ src/pipelines/web/web_pipeline.rs
@@ -2,5 +2,8 @@ use crate::cli::WebArgs;
 
 /// The web pipeline
 pub fn web_pipeline(args: WebArgs) {
-
+    println!("building web");
+    if args.serve {
+        println!("serving");
+    }
 }
