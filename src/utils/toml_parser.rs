9866b8e983b549dc99c965e850e77ad0880167c4
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-05-03 12:34:02 +0530

    package_name

--- src/utils/toml_parser.rs
+++ src/utils/toml_parser.rs
@@ -1,15 +1,17 @@
 use std::path::PathBuf;
 
-use toml::value::Table;
 use toml::Value;
+use toml::value::Table;
 
 use crate::*;
 
 const DEPENDENCIES_STR: &str = "dependencies";
 const VERSION_STR: &str = "version";
 const FEATURES_STR: &str = "features";
+const PACKAGE_STR: &str = "package";
 
 pub struct CargoToml {
+    package_name: String,
     path: PathBuf,
     value: Value,
 }
@@ -29,6 +31,7 @@ impl CargoToml {
 
     pub async fn from_file(path: PathBuf) -> Result<Self> {
         Ok(Self {
+            package_name: "".to_string(),
             value: Self::serialise(
                 &read(path.as_path())
                     .await
@@ -62,71 +65,89 @@ impl CargoToml {
         let mut cargo_toml: Value = toml::from_slice(bytes)?;
         {
             let cargo_toml = cargo_toml.as_table_mut().unwrap();
-            // get the [dependency] part
-            let dependency_table = {
-                let dependency_table = cargo_toml
-                    .entry(DEPENDENCIES_STR)
-                    .or_insert_with(|| Value::Table(Table::new()));
-                dependency_table.as_table_mut().with_context(|| {
-                    format!("[{dep}] must be a table. [{dep}]", dep = DEPENDENCIES_STR)
-                })?
-            };
-            // get the gxi dependency
-            let gxi_table = {
-                let gxi_table = dependency_table
-                    .entry("gxi")
-                    .or_insert_with(|| Value::Table(Table::new()));
-                // check if the value is a string
-                let gxi_table = if let Some(str) = gxi_table.as_str() {
-                    // if it is a string then convert it to a table
-                    // and move that string to as its version
-                    dependency_table["gxi"] = Value::Table({
-                        let mut table = Table::new();
-                        table
-                            .entry(VERSION_STR)
-                            .or_insert(Value::String(String::from(str)));
-                        table
-                    });
-                    &mut dependency_table["gxi"]
-                } else {
-                    gxi_table
-                };
-                gxi_table.as_table_mut().with_context(|| {
-                    "Expected table as {} or string as \"\" for the value of dependency gxi"
-                })?
-            };
-            //check props
+            //package_name check
             {
-                // check version
-                gxi_table
-                    .entry(VERSION_STR)
-                    .or_insert_with(|| Value::String(String::new()));
-                // check features
-                {
-                    let features = {
-                        let features = gxi_table
-                            .entry(FEATURES_STR)
-                            .or_insert_with(|| Value::Array(Vec::new()));
-                        features.as_array_mut().with_context(|| {
-                            "Expected array of strings as the value of features of dependency gxi"
-                        })?
+                // get the [package] part
+                let package_table = {
+                    let package_table = cargo_toml
+                        .entry(PACKAGE_STR)
+                        .or_insert_with(|| Value::Table(Table::new()));
+                    package_table.as_table_mut().with_context(|| {
+                        format!("[{pack}] must be a table. [{pack}]", pack = PACKAGE_STR)
+                    })?
+                };
+                if !package_table.contains_key("name") {
+                    bail!("expected name under table [package] in Cargo.toml")
+                }
+            }
+            //dependency_check
+            {
+                // get the [dependency] part
+                let dependency_table = {
+                    let dependency_table = cargo_toml
+                        .entry(DEPENDENCIES_STR)
+                        .or_insert_with(|| Value::Table(Table::new()));
+                    dependency_table.as_table_mut().with_context(|| {
+                        format!("[{dep}] must be a table. [{dep}]", dep = DEPENDENCIES_STR)
+                    })?
+                };
+                // get the gxi dependency
+                let gxi_table = {
+                    let gxi_table = dependency_table
+                        .entry("gxi")
+                        .or_insert_with(|| Value::Table(Table::new()));
+                    // check if the value is a string
+                    let gxi_table = if let Some(str) = gxi_table.as_str() {
+                        // if it is a string then convert it to a table
+                        // and move that string to as its version
+                        dependency_table["gxi"] = Value::Table({
+                            let mut table = Table::new();
+                            table
+                                .entry(VERSION_STR)
+                                .or_insert(Value::String(String::from(str)));
+                            table
+                        });
+                        &mut dependency_table["gxi"]
+                    } else {
+                        gxi_table
                     };
-                    // remove both web and desktop feature
-                    // because they'll be automatically be added by
-                    // the respected pipelines
+                    gxi_table.as_table_mut().with_context(|| {
+                        "Expected table as {} or string as \"\" for the value of dependency gxi"
+                    })?
+                };
+                //check props
+                {
+                    // check version
+                    gxi_table
+                        .entry(VERSION_STR)
+                        .or_insert_with(|| Value::String(String::new()));
+                    // check features
                     {
-                        let mut to_remove = Vec::new();
-                        for (i, val) in features.iter().enumerate() {
-                            let str = val
-                                .as_str()
-                                .with_context(|| "Values of feature array can only have strings")?;
-                            if str == DESKTOP_FEATURE || str == WEB_FEATURE {
-                                // features resizes when an element is removed so the index should be shifted
-                                to_remove.push(i - to_remove.len());
+                        let features = {
+                            let features = gxi_table
+                                .entry(FEATURES_STR)
+                                .or_insert_with(|| Value::Array(Vec::new()));
+                            features.as_array_mut().with_context(|| {
+                                "Expected array of strings as the value of features of dependency gxi"
+                            })?
+                        };
+                        // remove both web and desktop feature
+                        // because they'll be automatically be added by
+                        // the respected pipelines
+                        {
+                            let mut to_remove = Vec::new();
+                            for (i, val) in features.iter().enumerate() {
+                                let str = val
+                                    .as_str()
+                                    .with_context(|| "Values of feature array can only have strings")?;
+                                if str == DESKTOP_FEATURE || str == WEB_FEATURE {
+                                    // features resizes when an element is removed so the index should be shifted
+                                    to_remove.push(i - to_remove.len());
+                                }
                             }
-                        }
-                        for x in to_remove {
-                            features.remove(x);
+                            for x in to_remove {
+                                features.remove(x);
+                            }
                         }
                     }
                 }
@@ -139,20 +160,21 @@ impl CargoToml {
 #[test]
 fn test_parse_cargo_toml() -> Result<()> {
     use crate::CargoToml;
-     //no dependency
+    //no dependency
     {
-        let cargo_toml = CargoToml::serialise("".as_bytes())?;
+        let cargo_toml = CargoToml::serialise(format!("[{}]\nname=\"\"", PACKAGE_STR).as_bytes())?;
         assert_eq!(
             cargo_toml.to_string(),
             format!(
-                "[{}.gxi]\n{} = \"\"\n{} = []\n",
-                DEPENDENCIES_STR, VERSION_STR, FEATURES_STR
+                "[{}]\nname = \"\"\n[{}.gxi]\n{} = \"\"\n{} = []\n",
+                PACKAGE_STR, DEPENDENCIES_STR, VERSION_STR, FEATURES_STR
             )
         )
     }
     {
         let test_str = format!(
-            "[{dep}]\nk = \"\"\n\n[{dep}.gxi]\n{ver} = \"\"\n{fea} = []\n",
+            "[{}]\nname = \"\"\n\n[{dep}]\nk = \"\"\n\n[{dep}.gxi]\n{ver} = \"\"\n{fea} = []\n",
+            PACKAGE_STR,
             dep = DEPENDENCIES_STR,
             ver = VERSION_STR,
             fea = FEATURES_STR
@@ -163,9 +185,13 @@ fn test_parse_cargo_toml() -> Result<()> {
                 format!(
                     r#"
                         [{}]
+                        name = ""
+
+                        [{}]
                         k = ""
                         gxi = ""
                     "#,
+                    PACKAGE_STR,
                     DEPENDENCIES_STR
                 )
                     .as_bytes(),
@@ -176,11 +202,14 @@ fn test_parse_cargo_toml() -> Result<()> {
             let cargo_toml = CargoToml::serialise(
                 format!(
                     r#"
+                        [{}]
+                        name = ""
+
                         [{}]
                         k = ""
                         gxi = {{ {} = "" }}
                     "#,
-                    DEPENDENCIES_STR, VERSION_STR
+                    PACKAGE_STR, DEPENDENCIES_STR, VERSION_STR
                 )
                     .as_bytes(),
             )?;
@@ -191,10 +220,13 @@ fn test_parse_cargo_toml() -> Result<()> {
                 format!(
                     r#"
                         [{}]
+                        name = ""
+
+                        [{}]
                         k = ""
                         gxi = {{}}
                     "#,
-                    DEPENDENCIES_STR
+                    PACKAGE_STR, DEPENDENCIES_STR
                 )
                     .as_bytes(),
             )?;
@@ -204,18 +236,21 @@ fn test_parse_cargo_toml() -> Result<()> {
     // check extra props
     {
         let test_str = format!(
-            "[{}.gxi]\n{} = \"0.0.1\"\nhello = [\"foo\"]\n{} = []\n",
-            DEPENDENCIES_STR, VERSION_STR, FEATURES_STR
+            "[{}]\nname = \"\"\n[{}.gxi]\n{} = \"0.0.1\"\nhello = [\"foo\"]\n{} = []\n",
+            PACKAGE_STR, DEPENDENCIES_STR, VERSION_STR, FEATURES_STR
         );
         {
             let cargo_toml = CargoToml::serialise(
                 format!(
                     r#"
+                        [{}]
+                        name = ""
+
                         [{}.gxi]
                         {} = "0.0.1"
                         hello = [ "foo" ]
                     "#,
-                    DEPENDENCIES_STR, VERSION_STR
+                    PACKAGE_STR, DEPENDENCIES_STR, VERSION_STR
                 )
                     .as_bytes(),
             )?;
@@ -226,9 +261,12 @@ fn test_parse_cargo_toml() -> Result<()> {
                 format!(
                     r#"
                         [{}]
+                        name = ""
+
+                        [{}]
                         gxi = {{ {} = "0.0.1", hello = [ "foo" ] }}
                     "#,
-                    DEPENDENCIES_STR, VERSION_STR
+                    PACKAGE_STR, DEPENDENCIES_STR, VERSION_STR
                 )
                     .as_bytes(),
             )?;
@@ -240,18 +278,21 @@ fn test_parse_cargo_toml() -> Result<()> {
         let cargo_toml = CargoToml::serialise(
             format!(
                 r#"
+                    [{}]
+                    name = ""
+
                     [{}.gxi]
                     features = ["desktop","web","async","web","desktop"]
                 "#,
-                DEPENDENCIES_STR
+                PACKAGE_STR, DEPENDENCIES_STR
             )
                 .as_bytes(),
         )?;
         assert_eq!(
             cargo_toml.to_string(),
             format!(
-                "[{}.gxi]\n{} = [\"async\"]\n{} = \"\"\n",
-                DEPENDENCIES_STR, FEATURES_STR, VERSION_STR
+                "[{}]\nname = \"\"\n[{}.gxi]\n{} = [\"async\"]\n{} = \"\"\n",
+                PACKAGE_STR, DEPENDENCIES_STR, FEATURES_STR, VERSION_STR
             )
         );
     }
