dca3ed4d5baaa9f7d799fbe7f264556dca7595c9
Aniket Prajapati <prajapati.ani306@gmail.com>
2021-06-04 13:21:25 +0530

    public_dir arg

--- src/cli/web_args.rs
+++ src/cli/web_args.rs
@@ -12,6 +12,9 @@ pub struct WebArgs {
     /// start serving files at host:port
     #[clap(short, long, value_hint = ValueHint::Hostname)]
     pub serve: Option<String>,
+    /// public dir containing static files eg. css served at /
+    #[clap(lang = "public-dir", short, value_hint = ValueHint::DirPath, requires = "serve")]
+    pub public_dir: Option<PathBuf>,
     /// build on file change
     #[clap(short, long)]
     pub watch: bool,
@@ -22,7 +25,7 @@ pub struct WebArgs {
     #[clap(long)]
     pub release: bool,
     /// target dir for cargo builds.
-    #[clap(long = "target-dir", default_value = "target", value_hint = ValueHint::DirPath)]
+    #[clap(long = "target-dir", short, default_value = "target", value_hint = ValueHint::DirPath)]
     pub target_dir: PathBuf,
     /// output dir for cargo builds.
     #[clap(long = "output-dir", short, default_value = "target/.gxi", value_hint = ValueHint::DirPath)]
